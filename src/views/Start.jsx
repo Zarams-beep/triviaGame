a301361b9c76438f67d0dd647a92b7f629d70c1b
Chizaram <chukwumaobidivine87@gmail.com>
2024-08-10 10:39:49 -0700

    third

--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -9,7 +9,7 @@ const Start=()=>{
               
                     <div className="divFlex">
                     <p className="startContainer">HOW MUCH DO YOU
-                        <br /> <span style={{paddingLeft:'5rem'}}>KNOW QUIZ</span></p>
+                        <br /> <span className="spanStyle">KNOW QUIZ</span></p>
                     </div>
                     <div className="divFlex2">
                     <button className="startBtn">
